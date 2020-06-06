ed21ea5d05aee7830d62f2bf8e3364e6ae9f3175
楊明豪 <yangmh@yangminghaodeMacBook-Pro.local>
2020-06-06 14:38:18 +0800

    create the function of create_model, finish the Unit Testing of create_model

--- autoencoder.py
+++ autoencoder.py
@@ -3,6 +3,8 @@ from torchvision.datasets import MNIST
 from torchvision import transforms
 from torch.utils.data import DataLoader
 import matplotlib.pyplot as plt
+import torch.nn as nn
+import torch.optim as optim
 
 # def
 
@@ -17,7 +19,49 @@ def data_preparation(root='./data', batch_size=1000):
     return train_loader, test_loader
 
 
+def create_model(code_size, lr):
+    model = AUTOENCODER(code_size=code_size)
+    optimizer = optim.Adam(params=model.parameters(), lr=lr)
+    criterion = nn.MSELoss()
+    return model, optimizer, criterion
+
+# class
+
+
+class AUTOENCODER(nn.Module):
+    def __init__(self, code_size=2):
+        super(AUTOENCODER, self).__init__()
+        self.enc_l1 = nn.Linear(28*28, 256)
+        self.enc_l2 = nn.Linear(256, code_size)
+        self.dec_l1 = nn.Linear(code_size, 256)
+        self.dec_l2 = nn.Linear(256, 28*28)
+        self.sigmoid = nn.Sigmoid()
+        self.tanh = nn.Tanh()
+
+    def forward(self, x):
+        x = self.sigmoid(self.enc_l1(x))
+        x = self.tanh(self.enc_l2(x))
+        x = self.sigmoid(self.dec_l1(x))
+        x = self.sigmoid(self.dec_l2(x))
+        return x
+
+    def encoder(self, x):
+        x = self.sigmoid(self.enc_l1(x))
+        x = self.tanh(self.enc_l2(x))
+        return x
+
+    def decoder(self, x):
+        x = self.sigmoid(self.dec_l1(x))
+        x = self.sigmoid(self.dec_l2(x))
+        return x
+
+
 if __name__ == "__main__":
+    # parameters
+    lr = 0.001
+    niter = 100
+    code_size = 2
+
     # get dataloader
     train_loader, test_loader = data_preparation()
 
@@ -26,4 +70,5 @@ if __name__ == "__main__":
     plt.imshow(img, cmap='gray')
     plt.show()
 
-    
+    # create model, optimizer, criterion
+    model, optimizer, criterion = create_model(code_size=code_size, lr=lr)
